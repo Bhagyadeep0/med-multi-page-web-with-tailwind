f29eb627ec41a5bc85a9783d58293062808137d6
Bhagyadeep <b.ghosh0330@gmail.com>
2024-01-29 18:59:26 +0530

    local to remote

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// const form = document.querySelector('form');
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "username",
+//     Password: "password",
+//     To: 'them@website.com',
+//     From: "you@isp.com",
+//     Subject: "This is the subject",
+//     Body: "And this is the body"
+//   }).then(
+//     message => alert(message)
+//   );
+// }
