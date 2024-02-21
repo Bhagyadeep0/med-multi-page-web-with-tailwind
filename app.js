d97c6f75fcbd0a77e60143b25dda2418c31785c3
Bhagyadeep <b.ghosh0330@gmail.com>
2024-02-21 19:43:30 +0530

    Updated Files

---
+++ app.js
@@ -0,0 +1,23 @@
+  // Toggle mobile menu visibility
+  document.getElementById('mobile-menu-button').addEventListener('click', function () {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+});
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+
+
+// const form = document.querySelector('form');
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "username",
+//     Password: "password",
+//     To: 'them@website.com',
+//     From: "you@isp.com",
+//     Subject: "This is the subject",
+//     Body: "And this is the body"
+//   }).then(
+//     message => alert(message)
+//   );
+// }
