8845607947f7e7776b39552ef22b8e92cb82baae
Bhagyadeep <b.ghosh0330@gmail.com>
2024-02-21 21:02:00 +0530

    Updated files

--- app.js
+++ app.js
@@ -3,21 +3,9 @@
         document.getElementById('mobile-menu').classList.toggle('hidden');
     });
 
-    var scroll = new SmoothScroll('a[href*="#"]');
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true
+    });
 
-
-
-// const form = document.querySelector('form');
-// function sendEmail() {
-//   Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "username",
-//     Password: "password",
-//     To: 'them@website.com',
-//     From: "you@isp.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body"
-//   }).then(
-//     message => alert(message)
-//   );
-// }
+      
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,3 @@ module.exports = {
   },
   plugins: [],
 }
-// const form = document.querySelector('form');
-// function sendEmail() {
-//   Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "username",
-//     Password: "password",
-//     To: 'them@website.com',
-//     From: "you@isp.com",
-//     Subject: "This is the subject",
-//     Body: "And this is the body"
-//   }).then(
-//     message => alert(message)
-//   );
-// }
